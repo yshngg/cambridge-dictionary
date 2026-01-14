8d2121c12685120b45ed77e892c1933f9e326ce4
Yusheng Guo <yshngg@outlook.com>
2026-01-15 00:29:35 +0800

    feat: read words from file

--- e2e/word.spec.ts
+++ e2e/word.spec.ts
@@ -1,12 +1,19 @@
 import { test, expect } from "@playwright/test";
+import { readFileSync, writeFileSync } from "fs";
 
-test("test", async ({ page }) => {
-  await page.goto(
-    "https://dictionary.cambridge.org/dictionary/english-chinese-simplified/",
-  );
-  await page.getByRole("button", { name: "I Accept" }).click();
-  await page.getByRole("textbox", { name: "Search" }).click();
-  await page.getByRole("textbox", { name: "Search" }).fill("example");
-  await page.getByRole("button", { name: "Search", exact: true }).click();
-  await expect(page.locator("#page-content")).toContainText("example");
-});
+let content = readFileSync("e2e/words.txt", { encoding: "utf-8" });
+let wordlist = content.split("\n");
+for (let word of wordlist) {
+  word = word.trim();
+  if (word.length == 0) continue;
+  test(`test ${word}`, async ({ page }) => {
+    await page.goto(
+      "https://dictionary.cambridge.org/dictionary/english-chinese-simplified/",
+    );
+    await page.getByRole("button", { name: "I Accept" }).click();
+    await page.getByRole("textbox", { name: "Search" }).click();
+    await page.getByRole("textbox", { name: "Search" }).fill(word);
+    await page.getByRole("button", { name: "Search", exact: true }).click();
+    await expect(page.locator("#page-content")).toContainText(word);
+  });
+}
